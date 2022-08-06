60833cb167aa04ef9c33a84e5d61e409be997af6
Kwemange Nyagrowa <80250781+jeff254-kedjango@users.noreply.github.com>
2022-08-06 15:14:57 +0300

    Add files via upload

---
+++ backend/core/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
+    path('api/', include('blog_api.urls', namespace='blog_api')),
+    path('api/user/', include('users.urls', namespace='users')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
